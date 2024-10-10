9498a984c2d2b06a162098953fa7fcfe5490cd64
Aryan Mangal <aryan@aryans-air-2.wifi.local.cmu.edu>
2024-10-10 16:29:34 -0400

    Correcting Active Projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,19 +137,34 @@ $(document).ready(function(){
 	});
 
 	// Handle 'Experience' content
-	$('#experience').click(function(e) {
+	// $('#experience').click(function(e) {
 
+	// 	// If the div has already the class active, no need to reload the divs...
+	// 	if(!$(e.target).hasClass('active')) {
+	// 		// Update navbar
+	// 		clearActiveLinks();
+	// 		activateLink(e);
+
+	// 		// Hide other contents
+	// 		clearActiveDivs();
+
+	// 		// Show current content
+	// 		activateDiv('#experienceContent');
+	// 	}
+	// });
+
+	$('#experience').click(function(e) {
 		// If the div has already the class active, no need to reload the divs...
 		if(!$(e.target).hasClass('active')) {
 			// Update navbar
 			clearActiveLinks();
 			activateLink(e);
-
+	
 			// Hide other contents
 			clearActiveDivs();
-
+	
 			// Show current content
-			activateDiv('#experienceContent');
+			$('#experienceSection').show(); // Display the section when clicked
 		}
 	});
 
